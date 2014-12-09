c1e270b7708e771a15d5d9b54324552e1873e5b7
Lex Nederbragt <lex.nederbragt@bio.uio.no>
2014-12-09 11:26:04 +0100

    Made trf2gff into a stand-alone tool

--- trf2gff.py
+++ trf2gff.py
@@ -3,26 +3,22 @@
 
 # <codecell>
 
-fname = '/Users/alexajo/Desktop/temp_desktop/igv/454_ilm_mT_terminator_filter2/scf7180002555995.fasta.trf_out.txt'
-
-# <codecell>
-
-with open(fname) as fh:
+# really simple parsing of the TRF output
+# requires the -ngs flag set (available from TRF version 4.07b)
+# generates a GFF file
+# untested, use at your own risk
+import sys
+with open(sys.argv[1]) as fh:
     for line in fh:
         ele = line.strip().split(" ")
         if line.startswith('@'):
             seq_name = ele[0][1:]
         else:
-            [start, stop, period, copies, 
-             consensus_size, perc_match, perc_indels, 
-             align_score, perc_A, perc_C, perc_G, perc_T, 
+            [start, stop, period, copies,
+             consensus_size, perc_match, perc_indels,
+             align_score, perc_A, perc_C, perc_G, perc_T,
              entropy, cons_seq, repeat_seq, left_flank, right_flank] = ele
             gff_line = [seq_name, 'TRF', cons_seq + '_' + copies + '_copies',
                         start, stop, '.', '.', '.', 'Name='+ cons_seq + '_' + copies + '_copies']
-            # '_'.join(ele[0:15])
             print '\t'.join(gff_line)
-            
-
-# <codecell>
-
 
